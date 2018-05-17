45acea770a956e5d672a102e2bc57fda0c6e1ffc
bojourmoon <misssozolina@gmail.com>
2018-05-17 20:10:19 +0300

    klasespirmais uzdevums

---
+++ classdiena9/classdiena9/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classdiena9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            pirma pirmaisobj = new pirma();
+            //masina bmw = new masina pamats objekta izveidei. lai pieklutu klasei vajag izveidot objektu
+            //klases nosaukums objektanosaukums = new  klasesnosaukums()
+            int rezultats = pirmaisobj.saskaitisana(0, 0);
+
+            //otrs objekts
+            pirma pirmaisobj2 = new pirma();
+
+              pirmaisobj.skaitlis = 5;
+            pirmaisobj2.skaitlis = 9;
+
+            Console.WriteLine("izvade no pirm    " + pirmaisobj.skaitlis);
+            Console.WriteLine("izvade no pirm2      " + pirmaisobj2.skaitlis);
+
+
+
+            Console.WriteLine("sis naks no manas klases---" + rezultats);
+            Console.ReadLine();
+        }
+        
+    }
+}
+// objekti var nakt no vienas klases bet viens otru neietekme
---
+++ classdiena9/classdiena9/pirma.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classdiena9
+{
+    class pirma
+
+    {
+        public int skaitlis; //vienkars parasts mainigais kuram var pieklut no citam klasem.nav labais tonis
+       // public, private piejama vai nepieejama var pieklut arpus sis klases. 
+       public int saskaitisana(int a,int b)//parasta publiska funkcija
+        {
+
+            return a + b;
+        }
+
+        private void izvade()
+        {
+
+            Console.WriteLine("tadaa");
+            Console.ReadKey();
+        }
+      
+    }
+    
+}
+//katru reizi kad pieklustam klasei un izveidojam objektu vinam var pieskirt citu vertibu 
